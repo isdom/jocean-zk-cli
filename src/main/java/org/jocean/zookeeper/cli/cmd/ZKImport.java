5db14f4ec053ae75d23b22a450383eccb8ed3aec
ming.ma <isdom@qq.com>
2019-01-27 11:21:21 +0800

    规范代码格式

--- src/main/java/org/jocean/zookeeper/cli/cmd/ZKImport.java
+++ src/main/java/org/jocean/zookeeper/cli/cmd/ZKImport.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.jocean.zookeeper.cli.cmd;
 
@@ -23,27 +23,29 @@ import com.google.common.base.Charsets;
  */
 public class ZKImport implements ZKCliCommand {
 
+    @Override
     public String getAction() {
 		return "zkimport";
 	}
 
-	public String getHelp() {
+	@Override
+    public String getHelp() {
 		return "dump zk path and children's node content"
 				+ "\r\n\tUsage: zkimport [import filename] [to path]"
 			;
 	}
 
 	@Override
-	public String execute(final ZKCliContext ctx, String... args) throws Exception {
+	public String execute(final ZKCliContext ctx, final String... args) throws Exception {
         final CuratorFramework curator = ctx.getCuratorFramework();
         if ( null == curator ) {
             return "not connect to zk, use zkopen first";
         }
-        
+
         if (args.length < 2) {
             return "missing import filename or path arg\n" + getHelp();
         }
-        
+
         final UnitDescription root = loadYaml(args[0]);
         if (null!=root) {
             importNode(curator, args[1], root);
@@ -58,21 +60,20 @@ public class ZKImport implements ZKCliCommand {
         }
     }
 
-    private void importNode(final CuratorFramework curator, 
+    private void importNode(final CuratorFramework curator,
             final String path, final UnitDescription desc) throws Exception {
         final String createdPath = importContent(curator, path, desc);
-        for (UnitDescription child : desc.getChildren()) {
+        for (final UnitDescription child : desc.getChildren()) {
             importNode(curator, createdPath, child);
         }
     }
 
-    private String importContent(final CuratorFramework curator, 
+    private String importContent(final CuratorFramework curator,
             final String path, final UnitDescription desc) throws Exception {
         return curator.create()
                 .creatingParentsIfNeeded()
                 .withMode(CreateMode.PERSISTENT)
-                .forPath(path + "/" + desc.getName(), 
-                        genBytes(desc.getParameters()));
+                .forPath(path + "/" + desc.getName(), genBytes(desc.getParameters()));
     }
 
     private static final byte[] EMPTY_BYTES = new byte[0];
